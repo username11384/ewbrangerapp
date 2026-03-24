9d6a831a570366c4b178db8e08b751b20063f51e
Immanuel Lam <immanuel.c.lam@student.uts.edu.au>
2026-03-24 11:34:20 +1100

    Initial Commit

---
+++ ewbapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ewbapp
+//
+//  Created by Immanuel Lam on 24/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ewbapp/ewbappApp.swift
@@ -0,0 +1,17 @@
+//
+//  ewbappApp.swift
+//  ewbapp
+//
+//  Created by Immanuel Lam on 24/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct ewbappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
